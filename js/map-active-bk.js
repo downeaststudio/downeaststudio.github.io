53de6ae93e8a7f47682bc1432a6c9c638deb3ac4
Kevin Temp <kevintemp@helpdesk-lp-osx.local>
2018-08-28 10:45:54 -0400

    Site overhaul

---
+++ js/map-active-bk.js
@@ -0,0 +1,1 @@
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2877.18888349541!2d-69.62967498449585!3d43.85191357911496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cad98ee8cebd937%3A0xa3c9fd9ca0c533a2!2s6+Bridge+St%2C+Boothbay+Harbor%2C+ME+04538!5e0!3m2!1sen!2sus!4v1535140785777" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
